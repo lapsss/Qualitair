9434c4c7d45da5fd3042bed2b59b5449eecc3c02
B. Jauffret <jauffret@gmail.com>
2021-04-15 00:48:50 +0200

    Update Qualitair.ino
    Added GPS

--- Arduino/Qualitair/Qualitair.ino
+++ Arduino/Qualitair/Qualitair.ino
@@ -47,14 +47,15 @@ const int ON = LOW;
 //GPS SETUP
 
 
-// A sample NMEA stream.
-const char *gpsStream =
-  "$GPRMC,045103.000,A,3014.1984,N,09749.2872,W,0.67,161.46,030913,,,A*7C\r\n"
-  "$GPGGA,045104.000,3014.1985,N,09749.2873,W,1,09,1.2,211.6,M,-22.5,M,,0000*62\r\n"
-  "$GPRMC,045200.000,A,3014.3820,N,09748.9514,W,36.88,65.02,030913,,,A*77\r\n"
-  "$GPGGA,045201.000,3014.3864,N,09748.9411,W,1,10,1.2,200.8,M,-22.5,M,,0000*6C\r\n"
-  "$GPRMC,045251.000,A,3014.4275,N,09749.0626,W,0.51,217.94,030913,,,A*7D\r\n"
-  "$GPGGA,045252.000,3014.4273,N,09749.0628,W,1,09,1.3,206.9,M,-22.5,M,,0000*6F\r\n";
+#if    defined (ARDUINO_AVR_OLIMEXINO_32U4)
+  #define SERIAL  Serial1
+#elif defined (ARDUINO_AVR_OLIMEXINO_328)
+  #define SERIAL  Serial
+#else
+  #define SERIAL  Serial
+#endif
+
+#define BAUDRATE 9600 // this is the default baudrate of the GPS module
 
 // The TinyGPS++ object
 TinyGPSPlus gps;
@@ -286,9 +287,11 @@ void setup() {
   bme.begin();  
 
   // Init GPS : 
-   while (*gpsStream)
-    if (gps.encode(*gpsStream++))
-      displayInfo();
+#if    defined (ARDUINO_AVR_OLIMEXINO_32U4)
+  pinMode (8, OUTPUT);
+  digitalWrite (8, LOW);
+  SERIAL.begin(BAUDRATE);
+#endif
 }
 
 void loop() {
@@ -312,8 +315,9 @@ void loop() {
         httpPutPropertry(thingName,"Temperature",String(temp));
         httpPutPropertry(thingName,"Pressure",String(pres));
         // Refresh location : 
-        if (gps.encode(*gpsStream++))
-          displayInfo();
+      while (SERIAL.available() > 0)
+         if (gps.encode(SERIAL.read()))
+            displayInfo();
         httpPutPropertry(thingName,"Location","{\"latitude\":\""+String(gps.location.lat())+"\",\"longitude\":\""+gps.location.lng()+"\",\"units\":\"WGS84\"}");
          }
         }
