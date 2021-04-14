cbdd99e3519ea798227531a6445bf0c9529aef6c
B. Jauffret <jauffret@gmail.com>
2021-04-15 00:21:55 +0200

    Update Qualitair.ino

--- Arduino/Qualitair/Qualitair.ino
+++ Arduino/Qualitair/Qualitair.ino
@@ -11,6 +11,7 @@
 #include <HTTPClient.h>
 #include "Adafruit_CCS811.h"
 #include <Adafruit_BME280.h>
+#include <TinyGPS++.h>
 
 //////////////////////
 // WiFi Definitions //
@@ -41,6 +42,23 @@ const int ON = LOW;
 // this will set as the Accept header for all the HTTP requests to the ThingWorx server
 // valid values are: application/json, text/xml, text/csv, text/html (default)
 #define ACCEPT_TYPE "application/json"  
+
+
+//GPS SETUP
+
+
+// A sample NMEA stream.
+const char *gpsStream =
+  "$GPRMC,045103.000,A,3014.1984,N,09749.2872,W,0.67,161.46,030913,,,A*7C\r\n"
+  "$GPGGA,045104.000,3014.1985,N,09749.2873,W,1,09,1.2,211.6,M,-22.5,M,,0000*62\r\n"
+  "$GPRMC,045200.000,A,3014.3820,N,09748.9514,W,36.88,65.02,030913,,,A*77\r\n"
+  "$GPGGA,045201.000,3014.3864,N,09748.9411,W,1,10,1.2,200.8,M,-22.5,M,,0000*6C\r\n"
+  "$GPRMC,045251.000,A,3014.4275,N,09749.0626,W,0.51,217.94,030913,,,A*7D\r\n"
+  "$GPGGA,045252.000,3014.4273,N,09749.0628,W,1,09,1.3,206.9,M,-22.5,M,,0000*6F\r\n";
+
+// The TinyGPS++ object
+TinyGPSPlus gps;
+
 
 /////////////////////
 //Attempt to make a WiFi connection. Checks if connection has been made once per second until timeout is reached
@@ -186,6 +204,62 @@ int postToThing(String nameOfThing, String endPoint, String postBody){
         return httpCode;
 }
 
+
+// GPS Display info
+void displayInfo()
+{
+  Serial.print(F("Location: ")); 
+  if (gps.location.isValid())
+  {
+    Serial.print(gps.location.lat(), 6);
+    Serial.print(F(","));
+    Serial.print(gps.location.lng(), 6);
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F("  Date/Time: "));
+  if (gps.date.isValid())
+  {
+    Serial.print(gps.date.month());
+    Serial.print(F("/"));
+    Serial.print(gps.date.day());
+    Serial.print(F("/"));
+    Serial.print(gps.date.year());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F(" "));
+  if (gps.time.isValid())
+  {
+    if (gps.time.hour() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.hour());
+    Serial.print(F(":"));
+    if (gps.time.minute() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.minute());
+    Serial.print(F(":"));
+    if (gps.time.second() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.second());
+    Serial.print(F("."));
+    if (gps.time.centisecond() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.centisecond());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.println();
+}
+
+
+
+
 void setup() {
 
     pinMode(RED_LED, OUTPUT);
@@ -209,9 +283,12 @@ void setup() {
 
   // Wait for the sensor to be ready
   while(!ccs.available());
-
-
   bme.begin();  
+
+  // Init GPS : 
+   while (*gpsStream)
+    if (gps.encode(*gpsStream++))
+      displayInfo();
 }
 
 void loop() {
@@ -234,7 +311,10 @@ void loop() {
         httpPutPropertry(thingName,"Humidity",String(hum));
         httpPutPropertry(thingName,"Temperature",String(temp));
         httpPutPropertry(thingName,"Pressure",String(pres));
-        httpPutPropertry(thingName,"Location","{\"latitude\":\""+String(i)+"\",\"longitude\":\"0.0\",\"units\":\"WGS84\"}");
+        // Refresh location : 
+        if (gps.encode(*gpsStream++))
+          displayInfo();
+        httpPutPropertry(thingName,"Location","{\"latitude\":\""+String(gps.location.lat())+"\",\"longitude\":\""+gps.location.lng()+"\",\"units\":\"WGS84\"}");
          }
         }
       i++;
